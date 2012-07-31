dc7ec190b5496c1436d5df07c64c648bbc7dbcec
caoyarzom <caoyarzom@gmail.com>
2012-07-30 22:33:27 -0400

    update Clientes
    Se agrega galeria

---
+++ cliente.php
@@ -0,0 +1,47 @@
+ 
+<div class="orga-imaganes">
+                    <h2>Director</h2>
+                    <div>
+                        <img src="images/foto.jpg" />
+                    </div>
+                    <p>Nombre </p>
+<!--                    <p><a href="images/cv.pdf" class="bump">Ver CV</a></p>-->
+
+                </div>
+                <div class="orga-imaganes">
+                    <h2>Director</h2>
+                    <div>
+                        <img src="images/foto.jpg" />
+                    </div>
+                    <p>Nombre </p>
+<!--                    <p><a href="images/cv.pdf" class="bump">Ver CV</a></p>-->
+
+                </div>
+                <div class="orga-imaganes">
+                    <h2>Director</h2>
+                    <div>
+                        <img src="images/foto.jpg" />
+                    </div>
+                    <p>Nombre </p>
+<!--                    <p><a href="images/cv.pdf" class="bump">Ver CV</a></p>-->
+
+                </div>
+                <div class="orga-imaganes">
+                    <h2>Director</h2>
+                    <div>
+                        <img src="images/foto.jpg" />
+                    </div>
+                    <p>Nombre </p>
+<!--                    <p><a href="images/cv.pdf" class="bump">Ver CV</a></p>-->
+
+                </div>
+                <div class="orga-imaganes">
+                    <h2>Director</h2>
+                    <div>
+                        <img src="images/foto.jpg" />
+                    </div>
+                    <p>Nombre </p>
+<!--                    <p><a href="images/cv.pdf" class="bump">Ver CV</a></p>-->
+
+                </div>
+
