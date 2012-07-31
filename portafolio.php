9722e3dbb9c86d912df9243cd91d1d8696f5857a
caoyarzom <caoyarzom@gmail.com>
2012-07-31 01:01:08 -0400

    Update Portafolio - otros
    Se agrega el menu portafolio - se arregla problema con padding del
    #container - se agrega prefix free para los webkit

---
+++ portafolio.php
@@ -0,0 +1,25 @@
+<script>
+    $("a[rel=galeria_profe]").fancybox({
+        'transitionIn'		: 'elastic',
+        'transitionOut'		: 'elastic',
+        'titlePosition' 	: 'outside',
+        'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
+            return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+        }
+    });
+</script> 
+<h2>Portafolio Muebleria </h2>
+<div id="thumbs"> 
+    <a rel="galeria_profe" href="images/img_portafolio/1.JPG" title=""><img alt="" src="images/img_portafolio/p_s_1.jpg"  width="80" height="80"/></a>
+    <a rel="galeria_profe" href="images/img_portafolio/2.JPG" title=""><img alt="" src="images/img_portafolio/p_s_2.jpg" /></a>
+    <a rel="galeria_profe" href="images/img_portafolio/3.JPG" title=""><img alt="" src="images/img_portafolio/p_s_3.jpg" /></a>
+    <a rel="galeria_profe" href="images/img_portafolio/4.JPG" title=""><img alt="" src="images/img_portafolio/p_s_4.jpg" /></a>
+    <a rel="galeria_profe" href="images/img_portafolio/5.JPG" title=""><img alt="" src="images/img_portafolio/p_s_5.jpg" /></a>
+    <a rel="galeria_profe" href="images/img_portafolio/6.JPG" title=""><img alt="" src="images/img_portafolio/p_s_6.jpg" /></a>
+    <a rel="galeria_profe" href="images/img_portafolio/7.JPG" title=""><img alt="" src="images/img_portafolio/p_s_7.jpg" /></a>
+    <a rel="galeria_profe" href="images/img_portafolio/8.JPG" title=""><img alt="" src="images/img_portafolio/p_s_8.jpg" /></a>
+    <a rel="galeria_profe" href="images/img_portafolio/9.JPG" title=""><img alt="" src="images/img_portafolio/p_s_9.jpg" /></a>
+    <a rel="galeria_profe" href="images/img_portafolio/10.JPG" title=""><img alt="" src="images/img_portafolio/p_s_10.jpg" /></a>
+    <a rel="galeria_profe" href="images/img_portafolio/11.JPG" title=""><img alt="" src="images/img_portafolio/p_s_11.jpg" /></a>
+    <a rel="galeria_profe" href="images/img_portafolio/13.JPG" title=""><img alt="" src="images/img_portafolio/p_s_13.jpg" /></a>
+</div>
