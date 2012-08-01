28ea53a4b76afc9568e1c2bfdc25560adbe665db
caoyarzom <caoyarzom@gmail.com>
2012-08-01 02:30:19 -0400

    Update Menu Nosotros

---
+++ nosotros.php
@@ -0,0 +1,16 @@
+<div id="thumbs">
+    <p class="parrafo_Sub_orga">¿Quienes Somos?</p>
+    <p class="parrafo_orga">Somos una empresa dedicada al diseño y 
+        fabricación de muebles  y otros servicios.
+        Nuestra virtud es la responsabilidad y profesionalismo a la hora de 
+        realizar proyectos y servicios a nuestros clientes, generando gran satisfacción
+        por nuestro trabajo y nuestra calidad humana, ya que creemos es la base de la confianza.
+    </p>
+<p class="parrafo_Sub_orga">Misión Organizacional</p>
+    <p class="parrafo_orga">Seguir creciendo como empresa para poder ser una de 
+        las mas importantes en el mercado, siendo referentes del bueno diseño la
+        innovación  y el profesionalismo, Entregando mayores y mejores 
+        satisfacciones a nuestros clientes, esperando ser los mejores  en el ámbito profesional y personal
+
+    </p>
+</div>
