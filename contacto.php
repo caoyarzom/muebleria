e32876d7578b50ad3b627d1494c2021d4538a6bd
caoyarzom <caoyarzom@gmail.com>
2012-08-01 17:53:17 -0400

    Update Contacto
    Se agrega el formulario contacto!

---
+++ contacto.php
@@ -0,0 +1,35 @@
+
+<div id="cuerpoContacto">
+    <div id="contenedorContacto">
+        <div class="columnaContacto1">
+            <div class="cc1_txt">
+                <div class="textoContacto">
+                    <fieldset>
+                        <h3>HORARIOS DE ATENCION</h3>
+                    <p>Lunes a Viernes de</p>
+                    <p>9:00 a 18:00 hrs </p>
+                    </fieldset>
+                </div>
+            </div>
+        </div>
+        <div class="columnaContacto1" >
+            <form accept-charset="UTF-8"  method="post" name="formularioo" id="formulario">
+                <fieldset><h3>CONTACTENOS</h3>
+                    <label for="nombre">¿Cúal es su nombre?</label>
+                    <input type="text" id="nombre" name="nombre" required placeholder="Escriba su nombre acá" value=""/>
+                    <label for="mail">Correo Electrónico</label>
+                    <input type="mail" id="mail" name="mail" required placeholder="Escriba su e-mail" value =""/>
+                    <label for="asunto">Asunto</label>
+                    <input type="text" id="asunto" name="asunto"required placeholder="Escriba el Asunto aquì" value=""/>
+                    <label for="telefono">Teléfono</label>
+                    <input type="text" id="telefono" name="telefono" equired placeholder="Escriba su numero de Teléfono" value=""/>
+                    <label for="mensaje">Mensaje</label>
+                    <textarea name="mensaje" cols="20" name="mensaje" rows="5" id="mensaje"  required placeholder="Escriba su mensaje..."></textarea>
+                    <input type="submit" id="lanzaVentana" value="Enviar"/>
+                </fieldset>
+            </form>
+        </div>
+
+
+    </div>
+</div>
---
+++ enviarMail.php
@@ -0,0 +1,78 @@
+<?php
+
+$listaDeErrores = 'listaVacia';
+
+$nombre = $_POST['nombre'];
+$mail = $_POST['mail'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+$telefono = $_POST['telefono'];
+
+//echo"nombre" . $nombre . " mail " . $mail . " asunto " . $asunto . " mensaje " . $mensaje;
+//valido el mail
+
+if ($mail == "" or !preg_match("/^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/", $mail)) {
+
+    $listaDeErrores = "Se encontraron <span style='color:red;'>Errores</span><br>";
+    $listaDeErrores.="<ul>";
+    $listaDeErrores.="<li>El <span style='color:red;'>Correo</span> ingresado no es valido <span style='color:red;'>" . $mail . "</span></li>";
+}
+if ($telefono == "" or !is_numeric($telefono)) {
+    if ($listaDeErrores == "listaVacia") {
+        $listaDeErrores = "Se encontraron <span>Errores<span><br>";
+        $listaDeErrores.="<ul>";
+        $listaDeErrores.="<li>El <span style='color:red;'>Telefono</span> ingresado no es valido <span style='color:red;'>" . $telefono . "</span></li>";
+    } else {
+        $listaDeErrores.="<li>El <span style='color:red;'>Telefono</span> ingresado no es valido <span style='color:red;'>" . $telefono . "</span></li>";
+    }
+}
+if ($nombre == "") {
+    if ($listaDeErrores == "listaVacia") {
+        $listaDeErrores = "Se encontraron <span>Errores<span><br>";
+        $listaDeErrores.="<ul>";
+        $listaDeErrores.="<li>El campo <span style='color:red;'>Nombre</span> no puede quedar vacio </li>";
+    } else {
+        $listaDeErrores.="<li>El campo <span style='color:red;'>Nombre</span> no puede quedar vacio </li>";
+    }
+}
+if ($asunto == "") {
+    if ($listaDeErrores == "listaVacia") {
+        $listaDeErrores = "Se encontraron <span>Errores<span><br>";
+        $listaDeErrores.="<ul>";
+        $listaDeErrores.="<li>El campo <span style='color:red;'>Asunto</span> no puede quedar vacio </li>";
+    } else {
+        $listaDeErrores.="<li>El campo <span style='color:red;'>Asunto</span> no puede quedar vacio </li>";
+    }
+}
+if ($mensaje == "") {
+    if ($listaDeErrores == "listaVacia") {
+        $listaDeErrores = "Se encontraron <span>Errores<span><br>";
+        $listaDeErrores.="<ul>";
+        $listaDeErrores.="<li>El campo <span style='color:red;'>Mensaje</span> no puede quedar vacio </li>";
+    } else {
+        $listaDeErrores.="<li>El campo <span style='color:red;'>Mensaje</span> no puede quedar vacio </li>";
+    }
+}
+if ($listaDeErrores != "listaVacia") {
+    $listaDeErrores.="</ul>";
+    echo $listaDeErrores;
+    return;
+} else {
+    $header = 'From: ' . $mail . " \r\n";
+    $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+    $header .= "Mime-Version: 1.0 \r\n";
+    $header .= "Content-Type: text/plain";
+
+    $mensajeCompleto = "De : " . $nombre . "\n";
+    $mensajeCompleto .="Teléfono : " . $telefono . "\n";
+    $mensajeCompleto .="Mail: " . $mail . "\n";
+    $mensajeCompleto .="Asunto: " . $asunto . "\n\n\n";
+    $mensajeCompleto .=$mensaje;
+
+    if (mail("contacto@dev-websur.tk", $asunto, $mensajeCompleto, $header)) {
+        echo "<p style='text-align:center;'>Su mensaje ha sido enviado</p>";
+    } else {
+        echo "<p style='text-align:center;'>Hay problemas de comunicacion con el servidor</p>";
+    }
+}
+?>
