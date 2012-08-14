48d473c824e8bb80fb8ef080037845d47201c0ac
caoyarzom <caoyarzom@gmail.com>
2012-08-14 12:12:42 -0400

    contador_

--- visitas/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$bd_host = "localhost"; 
-$bd_usuario = "root"; 
-$bd_password = ""; 
-$bd_base = "cont_visitas"; 
-$con = mysql_connect($bd_host, $bd_usuario, $bd_password); 
-mysql_select_db($bd_base, $con); 
-?> 
